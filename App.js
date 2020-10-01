fc3b0abb85c79a22d893a5e8b68dd5ac74d9ec4e
Nick Peay <nickpeay@Nicks-MacBook-Pro-2.local>
2020-10-01 17:20:05 -0400

    Adds update to include bottom nav

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Icon } from "react-native-vector-icons";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text>This is a new mobile app!</Text>
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.bottomNav}>
+        <FontAwesome5
+          name="calendar-day"
+          style={{ fontSize: 34, color: "green" }}
+          solid
+        />
+        <FontAwesome5
+          name="calendar-alt"
+          style={{ fontSize: 34, color: "green" }}
+          solid
+        />
+        <FontAwesome5
+          name={"plus-circle"}
+          style={{ fontSize: 34, color: "green" }}
+          solid
+        />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  bottomNav: {
+    display: "flex",
+    flexDirection: "row",
+    height: "10%",
+    backgroundColor: "#ececec",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingLeft: "10%",
+    paddingRight: "10%",
   },
 });
